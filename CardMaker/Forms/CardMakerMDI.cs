2a460b80135f93a12ac026a64877a20dbbcbfa3c
Tim Stair <nhmkamv@gmail.com>
2015-11-03 07:50:23 -0800

    Bumped the A version.

--- CardMaker/Forms/CardMakerMDI.cs
+++ CardMaker/Forms/CardMakerMDI.cs
@@ -67,7 +67,7 @@ namespace CardMaker.Forms
 
             m_sBaseTitle = "Card Maker Beta " + Application.ProductVersion;
 #if UNSTABLE
-            m_sBaseTitle += "[UNSTABLE] V.A1";
+            m_sBaseTitle += "[UNSTABLE] V.A2";
 #endif
             m_sFileOpenFilter = "CMP files (*.cmp)|*.cmp|All files (*.*)|*.*";
 
