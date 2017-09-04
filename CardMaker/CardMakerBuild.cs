d0e8082572156fcc03a42bd3479ee6e1c345a828
Tim Stair <nhmkamv@gmail.com>
2017-09-04 13:23:43 -0700

    Bumped up alpha version to A6.

--- CardMaker/CardMakerBuild.cs
+++ CardMaker/CardMakerBuild.cs
@@ -7,7 +7,7 @@ namespace CardMaker
         public static string GetBuildSuffix()
         {
 #if UNSTABLE
-            return "[UNSTABLE] V.A5";
+            return "[UNSTABLE] V.A6";
 #else
             return string.Empty;
 #endif
