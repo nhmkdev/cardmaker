65e84f52814b2edfd67069ccd54a8a003b92feaf
Tim Stair <nhmkamv@gmail.com>
2019-06-12 22:00:14 -0700

    Fix for issue with text markups and spacing due to rotated transform string measurements. Switching back to the identity transform, measuring, and then restoring seems to have knocked out the issue.

--- CardMaker/CardMakerBuild.cs
+++ CardMaker/CardMakerBuild.cs
@@ -31,7 +31,7 @@ namespace CardMaker
         public static string GetBuildSuffix()
         {
 #if UNSTABLE
-            return "[UNSTABLE] V.A1";
+            return "[UNSTABLE] V.A2";
 #else
             return string.Empty;
 #endif
