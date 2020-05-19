f9ef5e1af6df21a48859bfaba05ce0235bcc1c45
Tim Stair <nhmkamv@gmail.com>
2020-05-18 19:56:03 -0700

    Updated copyright year.

--- CardMaker/Card/Import/ExcelReferenceReader.cs
+++ CardMaker/Card/Import/ExcelReferenceReader.cs
@@ -1,7 +1,7 @@
 ﻿////////////////////////////////////////////////////////////////////////////////
 // The MIT License (MIT)
 //
-// Copyright (c) 2019 Tim Stair
+// Copyright (c) 2020 Tim Stair
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
