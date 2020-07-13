a5098759499d07fcc787efc88274220edc4f3415
Tim Stair <nhmkamv@gmail.com>
2020-07-13 16:59:13 -0700

    Prep for release build.

--- CardMaker/CardMakerBuild.cs
+++ CardMaker/CardMakerBuild.cs
@@ -31,7 +31,7 @@ namespace CardMaker
         public static string GetBuildSuffix()
         {
 #if UNSTABLE
-            return "[UNSTABLE] V.A13Z";
+            return "[UNSTABLE] V.A13";
 #else
             return string.Empty;
 #endif
