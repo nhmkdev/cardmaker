839aa3ed28407b34c5146adebdb515f94e0e036e
Tim Stair <nhmkamv@gmail.com>
2021-01-18 10:02:44 -0800

    Update for release.
    Copyright year update and UNSTABLE flag removed to create 1.1.0.0

--- CardMaker/CardMakerBuild.cs
+++ CardMaker/CardMakerBuild.cs
@@ -1,7 +1,7 @@
 ﻿////////////////////////////////////////////////////////////////////////////////
 // The MIT License (MIT)
 //
-// Copyright (c) 2020 Tim Stair
+// Copyright (c) 2021 Tim Stair
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -22,7 +22,7 @@
 // SOFTWARE.
 ////////////////////////////////////////////////////////////////////////////////
 
-#define UNSTABLE
+//#define UNSTABLE
 
 namespace CardMaker
 {
