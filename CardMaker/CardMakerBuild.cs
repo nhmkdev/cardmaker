f0f7cfe225abd1fbf08b988547474ddc1b196762
Tim Stair <nhmkamv@gmail.com>
2022-09-12 18:41:04 -0700

    Prep for A12

--- CardMaker/CardMakerBuild.cs
+++ CardMaker/CardMakerBuild.cs
@@ -31,7 +31,7 @@ namespace CardMaker
         public static string GetBuildSuffix()
         {
 #if UNSTABLE
-            return "[UNSTABLE] V.A11";
+            return "[UNSTABLE] V.A12";
 #else
             return string.Empty;
 #endif
