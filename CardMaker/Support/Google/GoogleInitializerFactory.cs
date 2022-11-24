a445b46c3ea26fd26b666dffbbf4db8a41bd2d05
Tim Stair <nhmkamv@gmail.com>
2022-11-24 09:59:39 -0800

    Version bump / unstable. Updated google auth.
    Google auth recommends using the IssuedUtc field (easy change).

--- CardMaker/Support/Google/GoogleInitializerFactory.cs
+++ CardMaker/Support/Google/GoogleInitializerFactory.cs
@@ -69,7 +69,7 @@ namespace Support.Google
             {
                 AccessToken = AccessToken,
                 ExpiresInSeconds = TokenExpirationSeconds,
-                Issued = DateTime.Now
+                IssuedUtc = DateTime.UtcNow
             };
 
             var zUserCredential = new UserCredential(zAuthCodeFlow, Environment.UserName, zTokenResponse);
