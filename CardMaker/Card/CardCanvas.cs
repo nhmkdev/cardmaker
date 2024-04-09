ff320a5138ec089df2ca76f840f80801357e7efe
Tim Stair <nhmkamv@gmail.com>
2024-04-09 08:10:11 -0700

    Fix noexport, haha... oops

--- CardMaker/Card/CardCanvas.cs
+++ CardMaker/Card/CardCanvas.cs
@@ -88,7 +88,7 @@ namespace CardMaker.Card
             if (-1 != ActiveDeck.CardIndex && ActiveDeck.CardIndex < ActiveDeck.CardCount)
             {
                 e.Graphics.FillRectangle(Brushes.White, 0, 0, Size.Width, Size.Height);
-                CardExported = !m_zCardRenderer.DrawCard(0, 0, e.Graphics, ActiveDeck.CurrentLine, false, true);
+                CardExported = m_zCardRenderer.DrawCard(0, 0, e.Graphics, ActiveDeck.CurrentLine, false, true);
             }
         }
 
