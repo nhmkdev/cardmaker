430792385d926cf93710b5930a8ac820d8595e80
Tim Stair <nhmkamv@gmail.com>
2026-09-20 19:35:51 -0700

    bump build version for pre-release

--- CardMaker/CardMakerBuild.cs
+++ CardMaker/CardMakerBuild.cs
@@ -31,7 +31,7 @@ namespace CardMaker
         public static string GetBuildSuffix()
         {
 #if UNSTABLE
-            return "[UNSTABLE] V.A2";
+            return "[UNSTABLE] V.A3";
 #else
             return string.Empty;
 #endif
